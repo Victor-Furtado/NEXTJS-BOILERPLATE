2f3f01dfb32a4b280e7917c0a3160655a3a24480
Victor Furtado <victor.furtado@coden3.com.br>
2022-10-24 15:59:23 -0300

    build: airbnb implemented

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
+/* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
